c85b8f617593f2c0b34e303a081b4344626af6f1
GreenCookiePie <yeobernardwz@gmail.com>
2021-01-26 17:35:46 +0800

    docker done

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,12 @@ const app = require('./server/server')
 const { mongoIndex, mongoDocker } = require('./server/database')
 const PORT = process.env.PORT || 5000
 
+// local connection
 // mongoIndex()
 //     .then(() => app.listen(PORT, () => console.log(`Server runnning on PORT: ${PORT}`)))
 //     .catch((error) => console.logmongoose.set('useFindAndModify', false)(error.message))
 
+// docker connection
 mongoDocker()
     .then(() => app.listen(PORT, () => console.log(`Server runnning on PORT: ${PORT}`)))
     .catch((error) => console.logmongoose.set('useFindAndModify', false)(error.message))
