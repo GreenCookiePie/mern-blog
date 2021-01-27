c1391689d8e6ec8a6ac182f90d9802fe808de3c8
GreenCookiePie <yeobernardwz@gmail.com>
2021-01-27 13:58:25 +0800

    docker explained

--- backend/server/database.js
+++ backend/server/database.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 // mongo localhost
 exports.mongoIndex = async () => await mongoose.connect('mongodb://localhost:27017/mern-blog', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 
-// mongo docker - note: for docker BE to connect to docker DB, change 'localhost' to 'mongo'
-exports.mongoDocker = async () => await mongoose.connect('mongodb://mongo:27017/mongo-test')
+// mongo docker - note: for docker BE to connect to docker DB, change 'localhost' to service name
+exports.mongoDocker = async () => await mongoose.connect('mongodb://database:27017/mongo-test')
 
 // mongo localhost test
 const connect = async () => await mongoose.connect('mongodb://localhost:27017/mern-blog-test', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
